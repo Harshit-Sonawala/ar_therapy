7127fb6deab12162b6c4939a77ddbf7932d1d08f
Harshit-Sonawala <hssonawala@gmail.com>
2023-02-05 14:45:15 +0530

    loginscreen edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:provider/provider.dart';
 import 'providers/disorder_list_provider.dart';
 import 'providers/exercise_list_provider.dart';
 
 import 'screens/home_screen.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<DisorderList>(create: (context) => DisorderList()),
-          ChangeNotifierProvider<ExerciseList>(create: (context) => ExerciseList()),
-        ],
-        child: const ARTherapy(),
-      ),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<DisorderList>(create: (context) => DisorderList()),
+        ChangeNotifierProvider<ExerciseList>(create: (context) => ExerciseList()),
+      ],
+      child: const ARTherapy(),
+    ),
+  );
+}
 
 class ARTherapy extends StatelessWidget {
   const ARTherapy({Key? key}) : super(key: key);
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,27 @@
-import 'package:ar_therapy/widgets/custom_elevated_button.dart';
-import 'package:ar_therapy/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/custom_elevated_button.dart';
+import '../widgets/custom_text_button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _AccountScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _AccountScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+  bool _isObscured = true;
+
+  @override
+  void dispose() {
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,17 +54,24 @@ class _AccountScreenState extends State<LoginScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    const Text(
+                    Text(
                       'Login here,',
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
+                      style: Theme.of(context).textTheme.bodyText1,
                     ),
                     const SizedBox(height: 40),
                     TextField(
+                      controller: emailTextController,
                       style: Theme.of(context).textTheme.bodyText1,
                       cursorColor: Theme.of(context).primaryColor,
                       decoration: InputDecoration(
+                        labelText: 'Email Address',
+                        labelStyle: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                        floatingLabelStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
                         prefixIcon: Icon(
                           Icons.email,
                           color: Theme.of(context).primaryColor,
@@ -68,17 +87,36 @@ class _AccountScreenState extends State<LoginScreen> {
                           ),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        hintText: 'Email Address',
                       ),
                     ),
                     const SizedBox(height: 20),
                     TextField(
+                      obscureText: _isObscured,
+                      controller: passwordTextController,
                       style: Theme.of(context).textTheme.bodyText1,
+                      cursorColor: Theme.of(context).primaryColor,
                       decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                        floatingLabelStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
                         prefixIcon: Icon(
                           Icons.key,
                           color: Theme.of(context).primaryColor,
                           size: 24,
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () => {
+                            setState(() => _isObscured = !_isObscured),
+                          },
+                          icon: Icon(
+                            _isObscured ? Icons.visibility : Icons.visibility_off,
+                            color: Theme.of(context).primaryColor,
+                          ),
                         ),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
@@ -90,12 +128,15 @@ class _AccountScreenState extends State<LoginScreen> {
                           ),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        hintText: 'Password',
                       ),
                     ),
                     const SizedBox(height: 40),
                     CustomElevatedButton(
-                      onPressed: () => {},
+                      onPressed: () => {
+                        debugPrint(
+                          'Email: ${emailTextController.text.trim()}, Password: ${passwordTextController.text.trim()}',
+                        ),
+                      },
                       title: 'Submit',
                       mainAxisAlignment: MainAxisAlignment.center,
                     ),
