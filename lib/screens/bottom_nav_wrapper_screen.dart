8c60fbbe6cf4925f88da57e5301f8afed3cc35f1
Harshit Sonawala <hssonawala@gmail.com>
2023-02-17 20:00:28 +0530

    added BottomNavBar

--- lib/screens/bottom_nav_wrapper_screen.dart
+++ lib/screens/bottom_nav_wrapper_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import './home_screen.dart';
-import './account_screen.dart';
+import './login_screen.dart';
+// import './account_screen.dart';
 import './wip_screen.dart';
+import '../widgets/custom_card.dart';
 
 class BottomNavWrapperScreen extends StatefulWidget {
   const BottomNavWrapperScreen({super.key});
@@ -13,9 +15,9 @@ class BottomNavWrapperScreen extends StatefulWidget {
 class _BottomNavWrapperScreenState extends State<BottomNavWrapperScreen> {
   final List<Map<String, Object>> _screenData = const [
     {"page": HomeScreen()},
-    {"page": WipScreen(screenName: 'Browse')},
     {"page": WipScreen(screenName: 'Explore')},
-    {"page": AccountScreen()},
+    {"page": WipScreen(screenName: 'Search')},
+    {"page": LoginScreen()},
   ];
 
   int _selectedScreenIndex = 0;
@@ -24,26 +26,43 @@ class _BottomNavWrapperScreenState extends State<BottomNavWrapperScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screenData[_selectedScreenIndex]['page'] as Widget,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedScreenIndex,
-        onTap: (int newIndex) => {
-          setState(() {
-            _selectedScreenIndex = newIndex;
-          }),
-        },
-        backgroundColor: const Color(0xff2d2d2d),
-        showUnselectedLabels: true,
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.white,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Browse'),
-          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Account'),
-        ],
+      bottomNavigationBar: CustomCard(
+        margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+        padding: const EdgeInsets.all(6),
+        child: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              label: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Account',
+            ),
+          ],
+          currentIndex: _selectedScreenIndex,
+          onTap: (int newIndex) => {
+            setState(() {
+              _selectedScreenIndex = newIndex;
+            }),
+          },
+          type: BottomNavigationBarType.fixed,
+          showUnselectedLabels: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+        ),
       ),
     );
   }
